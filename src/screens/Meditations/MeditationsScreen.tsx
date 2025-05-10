65290cfdb3df4634900ad269011c19580a2f8e78
Peter Lucan <52499649+PeterX12@users.noreply.github.com>
2025-05-10 21:01:11 +0100

    Apply fullScreen style to MeditationsScreen and ProfileScreen for consistent layout

--- src/screens/Meditations/MeditationsScreen.tsx
+++ src/screens/Meditations/MeditationsScreen.tsx
@@ -1,9 +1,10 @@
+import { AppStyles } from "constants/styles";
 import React from "react";
 import { View, Text } from "react-native";
 
 export default function MeditationsScreen() {
   return (
-    <View>
+    <View style={AppStyles.fullScreen}>
       <Text>Meditations Screen</Text>
     </View>
   );
--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,9 +1,10 @@
+import { AppStyles } from "constants/styles";
 import React from "react";
 import { View, Text } from "react-native";
 
 export default function ProfileScreen() {
   return (
-    <View>
+    <View style={AppStyles.fullScreen}>
       <Text>Profile Screen</Text>
     </View>
   );
