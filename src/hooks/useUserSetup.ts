9c1c49fee5bc18d34d92a266e3b6a4acb1ee9539
Peter Lucan <52499649+PeterX12@users.noreply.github.com>
2025-05-11 16:01:28 +0100

    feat: add custom hook useUserSetup for user profile initialization check

---
+++ src/hooks/useUserSetup.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const useUserSetup = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isUserSetup, setIsUserSetup] = useState(false);
+
+  useEffect(() => {
+    const checkUserSetup = async () => {
+      try {
+        const userData = await AsyncStorage.getItem("@user_profile");
+        setIsUserSetup(!userData);
+      } catch (error) {
+        console.error("Error checking user setup:", error);
+        setIsLoading(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkUserSetup();
+  }, []);
+  return { isLoading, isUserSetup };
+};
