2542bfbe473248d8af29b8da051ad50c6ace0824
Peter Lucan <52499649+PeterX12@users.noreply.github.com>
2025-05-22 01:33:05 +0100

    Onboarding screen work in progress

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -2,4 +2,6 @@ export const AppColors = {
   AppBackground: "#7C6E7F",
   tabBarActiveColor: "#606060",
   tabBarInactiveColor: "#000000",
+
+  White: "#FFFFFF",
 };
--- src/screens/Onboarding/OnboardingScreen.tsx
+++ src/screens/Onboarding/OnboardingScreen.tsx
@@ -1,11 +1,52 @@
+import { AppColors } from "constants/colors";
 import { AppStyles } from "constants/styles";
-import { View, Text } from "react-native";
+import { View, Text, TextInput } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default function OnboardingScreen() {
   return (
-    <View style={AppStyles.fullScreen}>
-      <Text>Welcome to the Stoic in You</Text>
-      <Text>Please enter your name and date of birth.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to the Stoic in You</Text>
+      <Text style={styles.subtitle}>
+        Please enter your name and date of birth
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.inputText}>Full Name</Text>
+        <TextInput style={styles.input} placeholder="Enter your name" />
+      </View>
     </View>
   );
 }
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: AppColors.AppBackground,
+    paddingHorizontal: 76,
+  },
+  title: {
+    color: AppColors.White,
+    fontSize: 24,
+  },
+  subtitle: {
+    color: AppColors.White,
+    fontSize: 16,
+    marginVertical: 30,
+  },
+  inputContainer: { width: "100%" },
+  inputText: {
+    color: AppColors.White,
+    fontSize: 16,
+  },
+  input: {
+    backgroundColor: AppColors.White,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    fontSize: 16,
+    marginTop: 8,
+  },
+});
