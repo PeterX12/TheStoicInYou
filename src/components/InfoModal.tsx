f4d42af0f318714783101d3bb89a6782a9f7c8b5
Peter Lucan <52499649+PeterX12@users.noreply.github.com>
2025-08-20 23:08:48 +0100

    Added Modal and some cleanup

--- src/components/InfoModal.tsx
+++ src/components/InfoModal.tsx
@@ -1,26 +1,84 @@
-import { Modal } from "react-native";
+import Ionicons from "@expo/vector-icons/build/Ionicons";
+import { AppColors } from "constants/colors";
+import React from "react";
+import { Modal, View, Text, StyleSheet, Pressable } from "react-native";
 
 interface InfoModalProps {
   isVisible: boolean;
   onClose?: () => void;
-  title: string;
   content: string;
-  showCloseButton?: boolean;
 }
 
-const InfoModal = ({
-  isVisible,
-  onClose,
-  title,
-  content,
-  showCloseButton = true,
-}: InfoModalProps) => {
+const InfoModal = ({ isVisible, onClose, content }: InfoModalProps) => {
   return (
     <Modal
       visible={isVisible}
       transparent={true}
       animationType="fade"
       onRequestClose={onClose}
-    ></Modal>
+    >
+      <View style={styles.overlay}>
+        <View style={styles.modalContent}>
+          <View style={styles.iconRow}>
+            <View style={styles.centerIcon}>
+              <Ionicons
+                name="information-circle-outline"
+                size={24}
+                color={AppColors.Black}
+              />
+            </View>
+            <Pressable style={styles.closeButton} onPress={onClose}>
+              <Ionicons
+                name="close-outline"
+                size={24}
+                color={AppColors.Black}
+              />
+            </Pressable>
+          </View>
+          <Text style={styles.content}>{content}</Text>
+        </View>
+      </View>
+    </Modal>
   );
 };
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,0.4)",
+  },
+  modalContent: {
+    backgroundColor: AppColors.White,
+    borderRadius: 8,
+    padding: 16,
+    paddingBottom: 32,
+    alignItems: "center",
+    marginHorizontal: 16,
+  },
+  content: {
+    fontSize: 16,
+    color: AppColors.Black,
+    textAlign: "center",
+  },
+  iconRow: {
+    marginBottom: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    position: "relative",
+    justifyContent: "center",
+  },
+  centerIcon: {
+    flex: 1,
+    alignItems: "center",
+  },
+  closeButton: {
+    position: "absolute",
+    right: 0,
+    zIndex: 1,
+  },
+});
+
+export default InfoModal;
--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,15 +1,18 @@
 import AppBar from "@components/AppBar";
 import Button from "@components/Button";
 import HourglassIcon from "@components/icons/HourglassIcon";
+import InfoModal from "@components/InfoModal";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { AppColors } from "constants/colors";
 import { AppStyles } from "constants/styles";
-import React from "react";
-import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { View, Text, ScrollView, Pressable } from "react-native";
 
 export default function ProfileScreen() {
   const navigation = useNavigation();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
     <View style={{ flex: 1 }}>
       <AppBar
@@ -24,11 +27,27 @@ export default function ProfileScreen() {
         contentContainerStyle={[AppStyles.fullScreen, { padding: 0 }]}
       >
         <HourglassIcon size={250} />
+        <Pressable onPress={() => setIsModalVisible(!isModalVisible)}>
+          <Ionicons
+            name="information-circle-outline"
+            size={24}
+            color={AppColors.White}
+          />
+        </Pressable>
+
         <Text>Memento Mori. Your time is limited</Text>
         <Text>Memento Vivere. Use it wisely</Text>
         <Text> Here’s a reminder of the time you have to live fully:</Text>
         <Text>Your time: 72 years, 11 months, 54 days</Text>
         <Text>Today matters</Text>
+
+        <InfoModal
+          isVisible={isModalVisible}
+          content={
+            "Your remaining time is based on average life expectancy. This number is only an estimate, life can be shorter or longer. Its purpose is not to predict your death, but to remind you to live with intention today."
+          }
+          onClose={() => setIsModalVisible(false)}
+        />
       </ScrollView>
     </View>
   );
